c7115c864d8c9655b75b4afd01cd53fd475bc0a1
Izabela Wolf <izabelawolfm@gmail.com>
2022-01-22 14:38:38 +0100

    Add active class to targetArticle

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function titleClickHandler(){
+event.preventDefaul();
 console.log('Link was clicked!');
 console.log(event);
+const clickedElement = this;
 
 /* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
@@ -11,7 +13,7 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-/* [IN PROGRESS] add class 'active' to the clicked link */
+/* [DONE] add class 'active' to the clicked link */
 console.log('clickedElement:', clickedElement);
 clickedElement.classList.add('active');
 
@@ -23,11 +25,15 @@ for(let activeArticle of activeArticles){
 	console.log('activeArticleRemoved');
 }
 
-  /* get 'href' attribute from the clicked link */
+/* get 'href' attribute from the clicked link */
+const articleSelector = clickedElement.getAttribute('href');
+console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
+/* find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
+/* add class 'active' to the correct article */
+targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
